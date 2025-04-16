bb17cf6dce64a05475b9d798f63714fecc307e0e
Rafael <rafaelbrandaobastos@gmail.com>
2025-04-16 17:23:57 -0300

    backend/changes

--- RestAPI/src/main/java/com/example/restapi/api/controller/UserController.java
+++ RestAPI/src/main/java/com/example/restapi/api/controller/UserController.java
@@ -1,30 +1,61 @@
 package com.example.restapi.api.controller;
+
 import com.example.restapi.api.model.User;
 import com.example.restapi.service.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService){
         this.userService = userService;
     }
 
-    @GetMapping("/user")
-    public User getUser(@RequestParam String username) {
-        Optional user = userService.getUser(username);
-        if(user.isPresent()) {
-            return (User) user.orElse(null);
+    //POST /users/username="joao"&password="123"
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestParam String username, @RequestParam String password) {
+        boolean created = userService.createUser(username, password);
+        if (created) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("Usuário registrado com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe, senha atualizada com sucesso.");
         }
-        return null;
     }
+
+    //POST /users/username="joao"&password="123"
+    @PostMapping("/login")
+    public ResponseEntity<String> checkUser(@RequestParam String username, @RequestParam String password) {
+        boolean exist = userService.checkUser(username, password);
+        if (exist) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Logado com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Não foi possivel autenticar com sucesso.");
+        }
+    }
+
+
+    //GET /users/{userId}
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Integer id){
+        Optional<User> user = userService.getUser(id);
+        return (User) user.orElse(null);
+    }
+
+    //GET /users/exists/{username}
+    @GetMapping("/exists/{username}")
+    public ResponseEntity<Boolean> userExists(@PathVariable String username) {
+        boolean exists = userService.getUserByUsername(username).isPresent();
+        return ResponseEntity.ok(exists); // sempre retorna 200 com true/false
+    }
+
 }
--- RestAPI/src/main/java/com/example/restapi/service/UserService.java
+++ RestAPI/src/main/java/com/example/restapi/service/UserService.java
@@ -1,7 +1,7 @@
 package com.example.restapi.service;
-import org.springframework.stereotype.Service;
 
 import com.example.restapi.api.model.User;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,23 +14,57 @@ public class UserService {
     private List<User> userList;
 
     public UserService() {
-
         userList = new ArrayList<>();
+        User user0 = new User(0,"admin",  "admin");
+        User user1 = new User(1,"Rafa",  "123");
+        User user2 = new User(2,"Caio",  "123");
 
-        User user = new User( "Rafa", "123");
-        User user2 = new User( "Caio", "123");
-
-        userList.addAll(Arrays.asList(user, user2));
+        userList.addAll(Arrays.asList(user0,user1,user2));
     }
 
-    public Optional<User> getUser(String username) {
-        Optional optional = Optional.empty();
-        for(User user : userList) {
-            if(user.getName().equals(username)) {
+    public Optional<User> getUser(Integer id) {
+        Optional<User> optional = Optional.empty();
+        for (User user: userList) {
+            if(id == user.getId()){
                 optional = Optional.of(user);
                 return optional;
             }
         }
         return optional;
     }
+
+    //AUXILIAR DO LOGIN
+    public boolean checkUser(String username, String password) {
+        for (User user : userList) {
+            if (user.getName().equals(username) && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean createUser(String username, String password) {
+        for (User user : userList) {
+            if (user.getName().equals(username)) {
+                // Se o usuário existe, atualiza a senha
+                user.setPassword(password);
+                return false;
+            }
+        }
+        // Se o usuário não existe, cria um novo usuário com novo id
+        int newId = userList.size();
+        User newUser = new User(newId, username, password);
+        userList.add(newUser);
+        return true;
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        for (User user : userList) {
+            if (user.getName().equals(username)) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
+
 }
